56b97437cd4090b66b7a4497ae9df133b001ba8f
EmSH <emilie.haritchabelat@hotmail.fr>
2026-06-10 14:09:46 +0200

    fix

--- karma.conf.js
+++ karma.conf.js
@@ -1,11 +1,16 @@
 module.exports = function (config) {
   config.set({
+    frameworks: ['jasmine'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage')
     ],
+    client: {
+      jasmine: {},
+      clearContext: false
+    },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/bibliotheque-front'),
       subdir: '.',
